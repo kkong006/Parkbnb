6e5d906a0764b357da97282191c8385d4768fbd4
qlyu001 <qlyu001@ucr.edu>
2017-05-20 13:04:30 -0700

    update welcome slide 1 and welcome slide 2

---
+++ parkbnb/app/src/main/java/edu/ucr/cutiehack/parkbnb/MainActivity.java
@@ -0,0 +1,37 @@
+package edu.ucr.cutiehack.parkbnb;
+
+/**
+ * Created by nellylyu on 5/20/17.
+ */
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        findViewById(R.id.btn_play_again).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // We normally won't show the welcome slider again in real app
+                // but this is for testing
+                PrefManager prefManager = new PrefManager(getApplicationContext());
+
+                // make first time launch TRUE
+                prefManager.setFirstTimeLaunch(true);
+
+                startActivity(new Intent(MainActivity.this, WelcomeActivity.class));
+                finish();
+            }
+        });
+    }
+}
