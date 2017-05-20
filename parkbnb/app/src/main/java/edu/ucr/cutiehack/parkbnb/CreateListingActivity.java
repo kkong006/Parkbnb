a9715106a62e1de09b2c2ec0bd07668d84f6ba73
mschaffrath <mscha008@ucr.edu>
2017-05-20 10:30:32 -0700

    added basic map

--- parkbnb/app/src/main/java/edu/ucr/cutiehack/parkbnb/CreateListingActivity.java
+++ parkbnb/app/src/main/java/edu/ucr/cutiehack/parkbnb/CreateListingActivity.java
@@ -2,12 +2,58 @@ package edu.ucr.cutiehack.parkbnb;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class CreateListingActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.ArrayList;
+
+public class CreateListingActivity extends AppCompatActivity implements OnMapReadyCallback{
+
+    private static final String TAG = "CreateListingsMap";
+
+    private GoogleMap mMap;
+    private DatabaseReference ref;
+    private Button btCreateListing;
+    private ArrayList parkingList;
+    private LatLng currLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_listing);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+/*
+        btCreateListing = (Button) findViewById(R.id.bt_create_listing);
+        btCreateListing.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(getBaseContext(), CreateListing.class);
+                startActivity(i);
+            }
+        });
+*/
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        mMap = googleMap;
+
+        Log.w(TAG, "onMapReady");
+    }
+
+    @Override
+    public void onBackPressed() {
+        this.moveTaskToBack(true);
     }
 }
